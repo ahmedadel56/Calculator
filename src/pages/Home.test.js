468c8e91ffe9f10979793b103eb7a45b8c6cd1b2
ahmedadel56 <ahmdadl563@gmail.com>
2021-11-04 18:28:53 +0200

    Test pages

---
+++ src/pages/Home.test.js
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+import Home from './Home';
+
+describe('<Home />', () => {
+  it('it should match snapshot', () => {
+    const tree = renderer.create(<Home />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/pages/Quotes.test.js
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+import Quotes from './Quotes';
+
+describe('<Quotes />', () => {
+  it('it should match snapshot', () => {
+    const tree = renderer.create(<Quotes />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
